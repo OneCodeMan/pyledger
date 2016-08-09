085f1370fb7be948c06a368c5809c778037e4259
Dave Gumba <dave_gumba@yahoo.com>
2016-08-09 14:27:46 -0400

    Bare minimum done

--- ledger.py
+++ ledger.py
@@ -1,11 +1,54 @@
+"""
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//     Code Buddha blesses this code to be bug free.
+//
+"""
 import csv
+import re
+import glob
 from tabulate import tabulate
 
 class Ledger:
 
 	def __init__(self):
 		self._total = 0.0
-		self._name = "myledger.txt"
+
+		# Look for all text files and assign new ledger instance as
+		# the previous ledger file number + 1. 
+		# Eg: If myledger2.txt exists, then create myledger3.txt
+		# When used again.
+		# If first time being used, just start off with myledger1.txt
+		existing_ledgers = []
+		for ledger in glob.glob("*.txt"):
+			existing_ledgers.append(ledger)
+
+		if existing_ledgers:
+			last_ledger = int(re.findall(r'\d+', existing_ledgers[-1])[0])
+			self._name = "myledger{}.txt".format(last_ledger+1)
+		else:
+			self._name = "myledger1.txt"
 
 		# Create csv
 		with open(self._name, "w") as f:
--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import ledger as l
-import os
 import sys
+import time
 
-if os.path.exists("myledger.txt"):
-	print("Ledger already exists!")
 
-	#Extract csv contents of already existing file
-	#with open("myledger.txt", "w") as f:
+print("Creating new ledger...")
+time.sleep(2)
 
-else:
-	print("Creating new ledger")
+led = l.Ledger()
+running = True
 
-	led = l.Ledger()
-	running = True
+while running:
+	print("(Enter 0 0 to quit)")
+	item, price = input("Enter an item and price, separated by a space. (eg: item1 10):\n").split()
 
-	while running:
-		print("(Enter 0 0 to quit)")
-		item, price = input("Enter an item and price, separated by a space. (eg: item1 10):\n").split()
+	if item == "0" and int(price) == 0:
+		print("Bye")
+		sys.exit()
 
-		if item == "0" and int(price) == 0:
-			sys.exit()
-
-		led.add(item, float(price))
-		led.display_ledger()
+	led.add(item, float(price))
+	led.display_ledger()
